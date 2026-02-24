0a179de1bbf5c1062984637a8520492007af6e56
Peter <peterfslade@me.com>
2026-02-24 14:30:40 +0000

    EDIT: add yet another hello

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("Peter"));
+        System.out.println(sayHelloToSomeone("Paul"));
         System.out.println(sayGoodbye());
     }
 
